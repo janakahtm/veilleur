fcdfd946b45f6b34f8605112d96bceb305c1b92c
Jethro <janakahtm@yahoo.com>
2026-06-02 16:32:55 -0400

    Le Veilleur recupere et affiche les actualit'es IA

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Le veilleur est en ligne !");
+require("dotenv").config();
+
+const cleApi = process.env.NEWS_API_KEY;
+
+console.log("Cle charge : ", cleApi);
+
+async function chercherNews() {
+    const url = `https://newsapi.org/v2/everything?q=intelligence artificielle&language=fr&sortBy=publishedAt&apiKey=${cleApi}`;
+    
+    const response = await fetch(url);
+    const donnees  = await response.json();
+
+    const articles = donnees.articles;
+
+    console.log(`📰 ${articles.length} articles trouvés :\n`);
+    articles.forEach((article, i) => {
+    console.log(`${i + 1}. ${article.title}`);
+    });
+
+}
+
+chercherNews();
